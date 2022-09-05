004b72fa6a77b9ce7f70711a5fb49eb8177bdc4a
Orcicorn <orcicorn@gmail.com>
2022-09-05 13:27:05 -0400

    Tweak spawns

---
+++ Contents/mods/SBTweaks3Cars/media/lua/server/Vehicles/SBTweak_Distribution_Filibuster.lua
@@ -0,0 +1,10 @@
+if VehicleZoneDistribution then -- check if the table exists for backwards compatibility
+    if getActivatedMod():contains("FRUsedCars") then
+        -- Remove Filibuster's Military vehicles since we're using KI5 instead
+        VehicleZoneDistribution.Military.vehicles["Base.hmmwvht"] = nil;
+        VehicleZoneDistribution.Military.vehicles["Base.hmmwvtr"] = nil;
+        VehicleZoneDistribution.Military.vehicles["Base.m35a2bed"] = nil;
+        VehicleZoneDistribution.Military.vehicles["Base.m35a2covered"] = nil;
+        VehicleZoneDistribution.Military.vehicles["Base.m151canvas"] = nil;
+    end
+end
---
+++ Contents/mods/SBTweaks3Cars/media/lua/server/Vehicles/SBTweak_Distribution_HarryTowTruck.lua
@@ -0,0 +1,10 @@
+if VehicleZoneDistribution then
+    if getActivatedMod():contains("HTowTruckFix") then
+        VehicleZoneDistribution.parkingstall.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 0.01};
+        VehicleZoneDistribution.trailerpark.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 5};
+        VehicleZoneDistribution.bad.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 1};
+        VehicleZoneDistribution.good.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 0.5};
+        VehicleZoneDistribution.junkyard.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 10};
+        VehicleZoneDistribution.trafficjamw.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 1};
+    end
+end
