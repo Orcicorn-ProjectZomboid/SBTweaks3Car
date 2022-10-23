237b1055758042eedcaa02bd6c9abd7a8b39ef41
Orcicorn <orcicorn@gmail.com>
2022-10-23 11:51:53 -0400

    Tweak Aquatsar club spawns

---
+++ Contents/mods/SBTweaks3Cars/media/lua/shared/SBTweak_Distribution_Aquaclub.lua
@@ -0,0 +1,37 @@
+require "VehicleZoneDefinition"
+if getActivatedMods():contains("amclub") then
+    -- Parking Stalls --
+    VehicleZoneDistribution.parkingstall.vehicles["Base.AMC_bmw_classic"] = {index = -1, spawnChance = 1};
+    VehicleZoneDistribution.parkingstall.vehicles["Base.AMC_bmw_custom"] = {index = -1, spawnChance = 1};
+    VehicleZoneDistribution.parkingstall.vehicles["Base.AMC_harley"] = {index = -1, spawnChance = 1};
+    VehicleZoneDistribution.parkingstall.vehicles["Base.AMC_quad"] = {index = -1, spawnChance = 0};
+
+    -- Trailer Parks --
+    VehicleZoneDistribution.trailerpark.vehicles["Base.AMC_quad"] = {index = -1, spawnChance = 3};
+    VehicleZoneDistribution.trailerpark.vehicles["Base.AMC_harley"] = {index = -1, spawnChance = 1};
+
+    -- Junk spawns --
+    VehicleZoneDistribution.junkyard.vehicles["Base.AMC_bmw_classic"] = {index = -1, spawnChance = 0};
+
+    -- Traffic Jams --
+    VehicleZoneDistribution.trafficjamn.vehicles["Base.AMC_harley"] = {index = -1, spawnChance = 0};
+    VehicleZoneDistribution.trafficjamn.vehicles["Base.AMC_bmw_classic"] = {index = -1, spawnChance = 0};
+
+    -- Special --
+    VehicleZoneDistribution.farm = VehicleZoneDistribution.farm or {}
+    VehicleZoneDistribution.farm.vehicles = VehicleZoneDistribution.farm.vehicles or {}
+    VehicleZoneDistribution.farm.vehicles["Base.AMC_harley"] = {index = -1, spawnChance = 0};
+    VehicleZoneDistribution.farm.vehicles["Base.AMC_quad"] = {index = -1, spawnChance = 0};
+    VehicleZoneDistribution.mccoy.vehicles["Base.AMC_quad"] = {index = -1, spawnChance = 3};
+    VehicleZoneDistribution.fossoil.vehicles["Base.AMC_quad"] = {index = -1, spawnChance = 1};
+    VehicleZoneDistribution.postal.vehicles["Base.AMC_bmw_classic"] = {index = -1, spawnChance = 0};
+    VehicleZoneDistribution.postal.vehicles["Base.AMC_quad"] = {index = -1, spawnChance = 0};
+
+    if VehicleZoneDistribution.sea then
+        VehicleZoneDistribution.parkingstall.vehicles["Base.TrailerAMCWaverunnerWithBody"] = {index = -1, spawnChance = 0};
+        VehicleZoneDistribution.sea.vehicles["Base.AMC_Waverunner"] = {index = -1, spawnChance = 0};
+        VehicleZoneDistribution.river.vehicles["Base.AMC_Waverunner"] = {index = -1, spawnChance = 0};
+        VehicleZoneDistribution.harbour.vehicles["Base.AMC_Waverunner"] = {index = -1, spawnChance = 0};
+        VehicleZoneDistribution.smallboat.vehicles["Base.AMC_Waverunner"] = {index = -1, spawnChance = 0};
+    end
+end
