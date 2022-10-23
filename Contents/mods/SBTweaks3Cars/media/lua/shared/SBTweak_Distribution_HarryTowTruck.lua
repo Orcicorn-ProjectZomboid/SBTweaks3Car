9bdd55f6b435fa9bf0cb4008f24b502c7a4c2f54
Orcicorn <orcicorn@gmail.com>
2022-10-23 11:43:36 -0400

    Doubled Tow Truck Spawns

--- Contents/mods/SBTweaks3Cars/media/lua/shared/SBTweak_Distribution_HarryTowTruck.lua
+++ Contents/mods/SBTweaks3Cars/media/lua/shared/SBTweak_Distribution_HarryTowTruck.lua
@@ -1,10 +1,10 @@
 if VehicleZoneDistribution then
     if getActivatedMods():contains("HTowTruckFix") then
-        VehicleZoneDistribution.parkingstall.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 0.5};
-        VehicleZoneDistribution.trailerpark.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 5};
+        VehicleZoneDistribution.parkingstall.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 1};
+        VehicleZoneDistribution.trailerpark.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 10};
         VehicleZoneDistribution.bad.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 1};
-        VehicleZoneDistribution.good.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 0.5};
-        VehicleZoneDistribution.junkyard.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 10};
+        VehicleZoneDistribution.good.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 1};
+        VehicleZoneDistribution.junkyard.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 15};
         VehicleZoneDistribution.trafficjamw.vehicles["Base.Chevalier_Rhino_TowTruck"] = {index = -1, spawnChance = 1};
     end
 end
