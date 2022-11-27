0cc45fd68c62fb95fd76a4b8a11776fd0cac75d1
Orcicorn <orcicorn@gmail.com>
2022-11-27 12:05:35 -0500

    Nerf lawnmower spawns

--- Contents/mods/SBTweaks3Cars/media/lua/shared/SBTweaks_Distribution_RidingMower.lua
+++ Contents/mods/SBTweaks3Cars/media/lua/shared/SBTweaks_Distribution_RidingMower.lua
@@ -1,21 +1,21 @@
 if VehicleZoneDistribution then
     if getActivatedMods():contains("RidingMower") then
         VehicleZoneDistribution.trailerpark.vehicles["Base.GoKart"] = {index = -1, spawnChance = 10};
-        VehicleZoneDistribution.trailerpark.vehicles["Base.RidingMower"] = {index = -1, spawnChance = 5};
-        VehicleZoneDistribution.trailerpark.vehicles["Base.RidingMower_Trailer"] = {index = -1, spawnChance = 3};
+        VehicleZoneDistribution.trailerpark.vehicles["Base.RidingMower"] = {index = -1, spawnChance = 1};
+        VehicleZoneDistribution.trailerpark.vehicles["Base.RidingMower_Trailer"] = {index = -1, spawnChance = 1};
 
         VehicleZoneDistribution.junkyard.vehicles["Base.GoKart"] = {index = -1, spawnChance = 5};
-        VehicleZoneDistribution.junkyard.vehicles["Base.RidingMower"] = {index = -1, spawnChance = 10};
+        VehicleZoneDistribution.junkyard.vehicles["Base.RidingMower"] = {index = -1, spawnChance = 1};
 
-        VehicleZoneDistribution.bad.vehicles["Base.RidingMower"] = {index = -1, spawnChance = 1};
-        VehicleZoneDistribution.bad.vehicles["Base.RidingMower_Trailer"] = {index = -1, spawnChance = 1};
+        VehicleZoneDistribution.bad.vehicles["Base.RidingMower"] = {index = -1, spawnChance = 0.5};
+        VehicleZoneDistribution.bad.vehicles["Base.RidingMower_Trailer"] = {index = -1, spawnChance = 0.5};
 
-        VehicleZoneDistribution.mccoy.vehicles["Base.GoKart"] = {index = -1, spawnChance = 2};
+        VehicleZoneDistribution.mccoy.vehicles["Base.GoKart"] = {index = -1, spawnChance = 5};
 
         -- If Filibuster installed, add to farm vehicles
         if getActivatedMods():contains("FRUsedCars") then
-            VehicleZoneDistribution.farm.vehicles["Base.RidingMower"] = {index = -1, spawnChance = 18};
-            VehicleZoneDistribution.farm.vehicles["Base.RidingMower_Trailer"] = {index = -1, spawnChance = 10};
+            VehicleZoneDistribution.farm.vehicles["Base.RidingMower"] = {index = -1, spawnChance = 10};
+            VehicleZoneDistribution.farm.vehicles["Base.RidingMower_Trailer"] = {index = -1, spawnChance = 6};
         end
 
         -- if TSARLib installed, add trailer to small trailers
